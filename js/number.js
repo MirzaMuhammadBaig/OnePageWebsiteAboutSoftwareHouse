bf4c02ac1b1c1e21819146aa7eabf550d7cdd70f
MirzaMuhammadBaig <mbaig4893@gmail.com>
2022-09-29 22:10:24 +0500

    Fainalize Our Project

--- js/number.js
+++ js/number.js
@@ -13,21 +13,18 @@ if (!startTime) {
    startTime = currentTime ;
 }
 
-//calculate the value to be used in calculating the number to be displayed
 const progress = Math.min((currentTime - startTime)/ duration, 1);
 
-//calculate what to be displayed using the value gotten above
 obj.innerHTML = Math.floor(progress * (lastVal - initVal) + initVal);
 
-//checking to make sure the counter does not exceed the last value (lastVal)
 if (progress < 1) {
    window.requestAnimationFrame(step);
 } else {
       window.cancelAnimationFrame(window.requestAnimationFrame(step));
    }
 };
-//start animating
-   window.requestAnimationFrame(step);
+
+window.requestAnimationFrame(step);
 }
 let text1 = document.getElementById('0101');
 let text2 = document.getElementById('0102');
